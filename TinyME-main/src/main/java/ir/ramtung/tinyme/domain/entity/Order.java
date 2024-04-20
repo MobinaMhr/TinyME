1cf5635c403b9b113227c17f21fe15e93bd46e77
MobinaMhr <mobinamehrazar81@gmail.com>
2024-04-20 14:08:34 +0330

    Add Stop Limit Order constructor

--- TinyME-main/src/main/java/ir/ramtung/tinyme/domain/entity/Order.java
+++ TinyME-main/src/main/java/ir/ramtung/tinyme/domain/entity/Order.java
@@ -24,8 +24,22 @@ public class Order {
     protected LocalDateTime entryTime = LocalDateTime.now();
     @Builder.Default
     protected OrderStatus status = OrderStatus.NEW;
-    protected int minimumExecutionQuantity;// add MEQ
+    protected int minimumExecutionQuantity;
+    protected int stopPrice;
 
+    public Order(long orderId, Security security, Side side, int quantity, int price, Broker broker, Shareholder shareholder, LocalDateTime entryTime, OrderStatus status, int minimumExecutionQuantity, int stopPrice) {
+        this.orderId = orderId;
+        this.security = security;
+        this.side = side;
+        this.quantity = quantity;
+        this.price = price;
+        this.entryTime = entryTime;
+        this.broker = broker;
+        this.shareholder = shareholder;
+        this.status = status;
+        this.minimumExecutionQuantity = minimumExecutionQuantity;
+        this.stopPrice = stopPrice;
+    }
     public Order(long orderId, Security security, Side side, int quantity, int price, Broker broker, Shareholder shareholder, LocalDateTime entryTime, OrderStatus status, int minimumExecutionQuantity) {
         this.orderId = orderId;
         this.security = security;
