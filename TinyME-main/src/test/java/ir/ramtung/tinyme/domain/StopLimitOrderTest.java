30411542be30c8b75751b14dd87e9c1516937074
MobinaMhr <mobinamehrazar81@gmail.com>
2024-04-21 15:54:22 +0330

    Add First Unit test for buy order

--- TinyME-main/src/test/java/ir/ramtung/tinyme/domain/StopLimitOrderTest.java
+++ TinyME-main/src/test/java/ir/ramtung/tinyme/domain/StopLimitOrderTest.java
@@ -1,17 +1,32 @@
 package ir.ramtung.tinyme.domain;
 
+import ir.ramtung.tinyme.config.MockedJMSTestConfig;
 import ir.ramtung.tinyme.domain.entity.*;
 import ir.ramtung.tinyme.domain.service.Matcher;
 import ir.ramtung.tinyme.domain.service.OrderHandler;
 import ir.ramtung.tinyme.messaging.EventPublisher;
+import ir.ramtung.tinyme.messaging.Message;
+import ir.ramtung.tinyme.messaging.event.OrderRejectedEvent;
+import ir.ramtung.tinyme.messaging.exception.InvalidRequestException;
+import ir.ramtung.tinyme.messaging.request.EnterOrderRq;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+
+@SpringBootTest
+@Import(MockedJMSTestConfig.class)
 public class StopLimitOrderTest {
+
     private static final int MAIN_BROKER_CREDIT = 100_000_000;
     private Security security;
     private Broker broker;
@@ -39,10 +54,26 @@ public class StopLimitOrderTest {
                 new Order(7, security, Side.SELL, 100, 15810, broker, shareholder)
         );
         orders.forEach(order -> orderBook.enqueue(order));
+
+        EnterOrderRq enterOrderRq = EnterOrderRq.createNewOrderRq(3, security.getIsin(), 2,
+                LocalDateTime.now(), Side.BUY, 100, 15900, broker.getBrokerId(),
+                shareholder.getShareholderId(), 0);
+        MatchResult result = security.newOrder(enterOrderRq, broker, shareholder, matcher);
+
     }
 
     @Test
-    void test() {
+    void check_if_buy_order_enqueues_to_inActive_queues() {
+        int testBrokerCredit = 20_000_000;
+        Broker testBroker = Broker.builder().credit(testBrokerCredit).build();
 
+        EnterOrderRq enterOrderRq = EnterOrderRq.createNewOrderRqWithStopPrice(3, security.getIsin(), 2,
+                LocalDateTime.now(), Side.BUY, 200, 15900, testBroker.getBrokerId(),
+                shareholder.getShareholderId(), 0, 15);
+
+        MatchResult result = security.newOrder(enterOrderRq, testBroker, shareholder, matcher);
+        assertThat(broker.getCredit()).isEqualTo(MAIN_BROKER_CREDIT);
+        assertThat(testBroker.getCredit()).isEqualTo(testBrokerCredit - (200 * 15900));
+        assertThat(orderBook.findByOrderIdForInactiveQueue(Side.BUY,2)).isNotNull();
     }
 }
