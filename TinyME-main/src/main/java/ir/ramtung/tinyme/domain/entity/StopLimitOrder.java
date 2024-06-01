a3f903a1f82d13b61f72c1a720d65f844d31c778
MobinaMhr <mobinamehrazar81@gmail.com>
2024-06-01 13:04:02 +0330

    Remove unsued StopLimitOrder constructora

--- TinyME-main/src/main/java/ir/ramtung/tinyme/domain/entity/StopLimitOrder.java
+++ TinyME-main/src/main/java/ir/ramtung/tinyme/domain/entity/StopLimitOrder.java
@@ -13,29 +13,42 @@ import java.time.LocalDateTime;
 public class StopLimitOrder extends Order {
     protected int stopPrice;
 
-    public StopLimitOrder(long orderId, Security security, Side side, int quantity, int price, Broker broker, Shareholder shareholder, LocalDateTime entryTime, OrderStatus status, int stopPrice) {
-        super(orderId, security, side, quantity, price, broker, shareholder, entryTime, status, 0);
+    public StopLimitOrder(long orderId, Security security, Side side,
+                          int quantity, int price, Broker broker,
+                          Shareholder shareholder, LocalDateTime entryTime,
+                          OrderStatus status, int stopPrice) {
+        super(orderId, security, side, quantity, price,
+                broker, shareholder, entryTime,
+                status, 0
+        );
         this.stopPrice = stopPrice;
     }
 
-    public StopLimitOrder(long orderId, Security security, Side side, int quantity, int price, Broker broker, Shareholder shareholder, LocalDateTime entryTime, int stopPrice) {
-        this(orderId, security, side, quantity, price, broker, shareholder, entryTime, OrderStatus.NEW, stopPrice);
-    }
-
-    public StopLimitOrder(long orderId, Security security, Side side, int quantity, int price, Broker broker, Shareholder shareholder, int stopPrice) {
-        this(orderId, security, side, quantity, price, broker, shareholder, LocalDateTime.now(), OrderStatus.NEW, stopPrice);
+    public StopLimitOrder(long orderId, Security security, Side side,
+                          int quantity, int price, Broker broker,
+                          Shareholder shareholder, int stopPrice) {
+        this(orderId, security, side, quantity, price,
+                broker, shareholder, LocalDateTime.now(),
+                OrderStatus.NEW, stopPrice
+        );
     }
 
     @Override
     public Order snapshot() {
-        return new StopLimitOrder(orderId, security, side, quantity, price, broker, shareholder, entryTime, OrderStatus.SNAPSHOT,
-                stopPrice);
+        return new StopLimitOrder(
+                orderId, security, side, quantity,
+                price, broker, shareholder, entryTime,
+                OrderStatus.SNAPSHOT, stopPrice
+        );
     }
 
     @Override
     public Order snapshotWithQuantity(int newQuantity) {
-        return new StopLimitOrder(orderId, security, side, newQuantity, price, broker, shareholder, entryTime, OrderStatus.SNAPSHOT,
-                stopPrice);
+        return new StopLimitOrder(
+                orderId, security, side, newQuantity,
+                price, broker, shareholder, entryTime,
+                OrderStatus.SNAPSHOT, stopPrice
+        );
     }
 
     public boolean canMeetLastTradePrice(int lastTradePrice){
